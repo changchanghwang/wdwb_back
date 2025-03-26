d66024d31973a9a8fd621d019a06e9293a5a8dcf
changchanghwang <hch950627@naver.com>
2025-03-26 21:14:20 +0900

    fix: 36

--- internal/services/investors/domain/model.go
+++ internal/services/investors/domain/model.go
@@ -7,7 +7,7 @@ import (
 
 type Investor struct {
 	ddd.Aggregate
-	Id           uuid.UUID `json:"id" gorm:"column:id; type:varchar(32); primaryKey"`
+	Id           uuid.UUID `json:"id" gorm:"column:id; type:varchar(36); primaryKey"`
 	Name         string    `json:"name" gorm:"type:varchar(255); not null;"`
 	CompanyName  string    `json:"companyName" gorm:"type:varchar(255); column:companyName; not null;"`
 	Cik          string    `json:"cik" gorm:"type:varchar(20);  not null;"`
