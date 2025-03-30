74bfa41f275b2e9c5a0e99212fe7f6f58b02c1a3
changchanghwang <hch950627@naver.com>
2025-03-30 14:15:50 +0900

    refactor: seperate with helper

--- internal/services/holdings/infrastructure/repositoryImpl.go
+++ internal/services/holdings/infrastructure/repositoryImpl.go
@@ -59,39 +59,3 @@ func (r *HoldingRepositoryImpl) Count(db *gorm.DB, conditions *HoldingQueryCondi
 
 	return int(count), nil
 }
-
-func applyConditions(conditions *HoldingQueryConditions) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if conditions == nil {
-			return db
-		}
-
-		if len(conditions.InvestorIds) > 0 {
-			db = db.Where("investorId IN ?", conditions.InvestorIds)
-		}
-
-		if len(conditions.Years) > 0 {
-			db = db.Where("year IN ?", conditions.Years)
-		}
-
-		if len(conditions.Quarters) > 0 {
-			db = db.Where("quarter IN ?", conditions.Quarters)
-		}
-
-		return db
-	}
-}
-
-func applyOptions(options *db.FindOptions, orderOptions *db.OrderOptions) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if options != nil {
-			db = db.Offset(options.Offset).Limit(options.Limit)
-		}
-
-		if orderOptions != nil {
-			db = db.Order(fmt.Sprintf("%s %s", orderOptions.OrderBy, orderOptions.Direction))
-		}
-
-		return db
-	}
-}
